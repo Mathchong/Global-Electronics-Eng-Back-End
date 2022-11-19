b4397381c8733c733dfff5755a59f6d668f20adb
Matheus Chong <matheus.chong@gmail.com>
2022-11-19 14:28:12 -0300

    Feat: bcrypt utils

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -1,5 +1,9 @@
 import bcrypt from "bcrypt";
 
-export function encrypt(data: string){
-    return bcrypt.hashSync(data, 10)
+export function encrypt(data: string) {
+  return bcrypt.hashSync(data, 10);
+}
+
+export function verifyHash(password: string, hash: string) {
+  return bcrypt.compareSync(password, hash);
 }
